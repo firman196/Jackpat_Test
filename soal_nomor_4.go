c474eb9b1f6771ebcdf46cf2a5353e5c7dfccc85
firman196 <firmanagussaputro970@gmail.com>
2023-06-03 21:41:16 +0700

    final

--- soal_nomor_4.go
+++ soal_nomor_4.go
@@ -1,14 +1,13 @@
 package jakpat
 
 import (
-	"fmt"
 	"time"
 )
 
 var andi = 12
 var beni = 14
 var ahmad = 6
-var startDate = time.Now().Format("2023-04-28")
+var startDate = time.Date(2023, 4, 28, 0, 0, 0, 0, time.UTC)
 
 func CheckMod(input int) bool {
 	if (input%andi == 0) && (input%beni == 0) && (input%ahmad == 0) {
@@ -17,15 +16,25 @@ func CheckMod(input int) bool {
 	return false
 }
 
-func SearchSimilarSchedule(input string) {
-	now := time.Now().Format(input + "12-31")
-	fmt.Println(now)
-	past, err := time.Parse(now, startDate)
-	if err != nil {
-		panic(err)
+func SearchSimilarSchedule(input int) []string {
+	now := time.Date(input, 12, 31, 0, 0, 0, 0, time.UTC)
+	var dayTotal int
+	if input == 2023 {
+		diff := now.Sub(startDate)
+		dayTotal = int(diff.Hours()) / 24
+	} else if input > 2023 {
+		dayTotal = 365
+	} else {
+		return []string{}
 	}
 
-	diff := time.Since(past)
-
-	fmt.Println(diff.Hours())
+	var arrDate []time.Time
+	for i := 0; i < dayTotal; i++ {
+		if CheckMod(i) {
+			hours := i * 24
+			startDate.Add(time.Duration(hours))
+			arrDate = append(arrDate, startDate)
+		}
+	}
+	return []string{}
 }
---
+++ soal_nomor_4_test.go
@@ -0,0 +1,7 @@
+package jakpat
+
+import "testing"
+
+func TestCaseSearchSimilarSchedule(t *testing.T) {
+	SearchSimilarSchedule(2023)
+}
